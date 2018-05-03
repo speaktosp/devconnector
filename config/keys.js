3b868e1a36c2a1a3653c8e05138a8874f14915af
Saravana Perumal Ramachandran <speaktosp@gmail.com>
2018-05-03 05:38:59 -0400

    Final Push with updated configs and package.json changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://massmutual:m%40ssmUtU%40l@ds111050.mlab.com:11050/mongoconnector"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
